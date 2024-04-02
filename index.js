60d43894acbf3fabe024a52fff17daea4b412e3e
David Cárdenas <santito500@live.com>
2024-04-01 23:52:07 -0500

    Cambios

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 import express from "express";
-import getData from "./routes/getdata.routes.js"
-import { PORT } from "./config.js";
+import getData from "./routes/getdata.routes.js";
+import { APP_PORT } from "./config.js";
 import path from "path";
-import cors from 'cors'
+import cors from "cors";
 import bodyParser from "body-parser";
+import fs from "fs";
+import http from "http";
+import https from "https";
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/carro_compras', getData);
+app.use("/carro", getData);
 
 app.use(express.static(path.resolve("public")));
 app.set("views", path.resolve("views"));
 app.set("view engine", "ejs");
 
+const options = {
+  key: fs.readFileSync("certs/privkey.pem"),
+  cert: fs.readFileSync("certs/cert.pem"),
+};
 
-
-app.listen(PORT);
-console.log("Server on port", PORT);
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(APP_PORT);
+console.log("Server on port", APP_PORT);
